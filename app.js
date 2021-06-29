e46c978142aadd2ca84738b73d76427fd1187fac
Alex  Rutan <alexrutan04@gmail.com>
2021-06-28 17:06:50 -0700

    last 4 exercises

--- app.js
+++ app.js
@@ -9,3 +9,29 @@ function randomNum() {
 } 
 
 $('h1').css('font-size', randomNum());
+
+
+let $newLi = $("<li>hello world!</li>");
+$('ol').append($newLi);
+
+
+$('aside').empty();
+let $newP = $("<p> I'm deeply sorry for listing</p>");
+$('aside').append($newP);
+
+
+let redValue;
+let blueValue;
+let greenValue;
+
+$(".form-control").on("change", function() {
+    redValue = Number($('input').eq(0).val());
+    blueValue = Number($('input').eq(1).val());
+    greenValue = Number($('input').eq(2).val());
+    $('body').css("background-color", `rgb(${redValue}, ${greenValue}, ${blueValue})`);
+});
+
+
+$("img").on("click", function(evt) {
+    evt.target.remove();
+});
